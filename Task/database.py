d1bbfaf03f90a12c480a566055f8cf49929d173a
inob <romansim03@mail.ru>
2024-02-11 17:12:26 +0300

    get database entries

--- Task/database.py
+++ Task/database.py
@@ -1,6 +1,25 @@
 import sqlite3
 import pandas as pd
 
+# conn = sqlite3.connect('posts.db')
+# data = pd.read_csv('posts.csv')
+# data.to_sql('posts', conn, if_exists='append', index = False)
+
+
+
 conn = sqlite3.connect('posts.db')
-data = pd.read_csv('posts.csv')
-data.to_sql('posts', conn, if_exists='append', index = False)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM posts")
+rows = cursor.fetchall()
+
+
+for row in rows:
+    
+    #row[1] - VK_ID
+    #row[2] - Text
+    #row[3] - date    
+    
+    print(row[3])
+
+conn.close()
