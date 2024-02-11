5aa205418ceab9fdd0f2e0d8c52a6f7c947c7b70
inob <romansim03@mail.ru>
2024-02-11 17:13:36 +0300

    Indexed data + search is working

--- Task/my_elasticsearch.py
+++ Task/my_elasticsearch.py
@@ -1,18 +1,41 @@
 from elasticsearch import Elasticsearch
 from passw import password
+import sqlite3
 es = Elasticsearch(
     [{'host': 'localhost', 'port': 9200, 'scheme': 'http'}],
     http_auth=('elastic', password)
 )
 
-response = es.search(
-    index="practice",
-    body={
-        "query": {
-            "match_all": {}
+# Тут мы индексируем данные в эластик, из sqlite
+# conn = sqlite3.connect('posts.db')
+# cursor = conn.cursor()
+
+# # Получаем данные из таблицы posts
+# cursor.execute("SELECT * FROM posts")
+# rows = cursor.fetchall()
+
+# count = 0
+# for row in rows:
+#     count+=1
+#     doc = {
+#         'id': count,
+#         'date': row[3],
+#         'text': row[2]
+#     }
+#     es.index(index='practice', body=doc)
+# conn.close()
+
+search_query = {
+    "query": {
+        "match": {
+            "id": "1500"
         }
     }
-)
+}
 
-for hit in response['hits']['hits']:
+# Отправляем запрос и получаем результат
+results = es.search(index="practice", body=search_query)
+
+# Обрабатываем результат
+for hit in results['hits']['hits']:
     print(hit['_source'])
