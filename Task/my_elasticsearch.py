82061e85a7961f3e2611807be1b805c9d3462d1c
inob <romansim03@mail.ru>
2024-02-12 18:51:34 +0300

    fix: id is written as a string

--- Task/my_elasticsearch.py
+++ Task/my_elasticsearch.py
@@ -1,41 +1,43 @@
+import sqlite3
 from elasticsearch import Elasticsearch
 from passw import password
-import sqlite3
+
 es = Elasticsearch(
     [{'host': 'localhost', 'port': 9200, 'scheme': 'http'}],
     http_auth=('elastic', password)
 )
 
-# Тут мы индексируем данные в эластик, из sqlite
-# conn = sqlite3.connect('posts.db')
-# cursor = conn.cursor()
 
-# # Получаем данные из таблицы posts
-# cursor.execute("SELECT * FROM posts")
-# rows = cursor.fetchall()
+conn = sqlite3.connect('posts.db')
+cursor = conn.cursor()
 
-# count = 0
-# for row in rows:
-#     count+=1
-#     doc = {
-#         'id': count,
-#         'date': row[3],
-#         'text': row[2]
-#     }
-#     es.index(index='practice', body=doc)
-# conn.close()
+cursor.execute("SELECT * FROM posts")
+rows = cursor.fetchall()
 
-search_query = {
-    "query": {
-        "match": {
-            "id": "1500"
-        }
+count = 0
+for row in rows:
+    count += 1
+    doc = {
+        'id': str(count),  
+        'date': row[3],
+        'text': row[2]
     }
-}
+    es.index(index='practice', id=doc['id'], body=doc)  
 
-# Отправляем запрос и получаем результат
-results = es.search(index="practice", body=search_query)
+conn.close()
 
-# Обрабатываем результат
-for hit in results['hits']['hits']:
-    print(hit['_source'])
+
+# search_query = {
+#     "query": {
+#         "match": {
+#             "id": "1500"
+#         }
+#     }
+# }
+
+# # Отправляем запрос и получаем результат
+# results = es.search(index="practice", body=search_query)
+
+# # Обрабатываем результат
+# for hit in results['hits']['hits']:
+#     print(hit['_source'])
