c38b008429f002f15fac025e4b673a8cdc46e8f6
inob <romansim03@mail.ru>
2024-02-12 18:52:09 +0300

    fix: can be deleted by id

--- Task/main.py
+++ Task/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, NotFoundError
 from passw import password
 import sqlite3
 
@@ -29,8 +29,11 @@ async def search(text: str):
     results = es.search(index="practice", body=search_query)
     return results['hits']['hits']
 
-
 @app.delete("/delete/{id}")
-async def delete(id: str):
-    es.delete(index="practice", id=id)
-    return {"detail": "Document deleted"}
+async def delete(id: int):
+    id = str(id)  # Преобразуем id в строку
+    if es.exists(index="practice", id=id):
+        es.delete(index="practice", id=id)
+        return {"detail": "Document deleted"}
+    else:
+        return {"detail": "Document does not exist"}
